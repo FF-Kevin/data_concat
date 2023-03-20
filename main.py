bbb126f8bcd7565e5a161303cdaee856aecda477
FF-Kevin <kevin@finlessfoods.com>
2023-03-20 12:03:10 -0700

    Added Recursion
    Added recursive loop to process multiple runs of data instead of data. Eliminate need of having to reopen script for every run

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from functions import merge
 
-parent_dir = input("What is the file path of the AMBR data?: ")
-merge.combine_all(parent_dir)
+rerun = True
+while rerun:
+    parent_dir = input("What is the file path of the AMBR data?: ")
+    merge.combine_all(parent_dir)
+    again = input("Do you have any more data? (Y/N):")
+    if again.upper() == 'N':
+        rerun = False
